60d6da9bc1a174ba594073ccda9178e72313cd45
Gabriela <gabilaras14@gmail.com>
2022-10-05 19:40:42 -0300

    Add interface

--- mini-autorizador/src/main/java/br/com/autorizador/services/AutorizadorService.java
+++ mini-autorizador/src/main/java/br/com/autorizador/services/AutorizadorService.java
@@ -3,70 +3,27 @@ package br.com.autorizador.services;
 import br.com.autorizador.exception.GenericBusinessException;
 import br.com.autorizador.model.Request.CartaoRequest;
 import br.com.autorizador.model.Request.TransacaoRequest;
-import br.com.autorizador.model.entity.Cartao;
-import br.com.autorizador.model.enums.ErroAutorizacaoEnum;
 import br.com.autorizador.model.response.CartaoResponse;
-import br.com.autorizador.repository.CartaoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
-@Service
-public class AutorizadorService {
+public interface AutorizadorService {
 
-    private static final BigDecimal VALOR_SALDO_INICIAL = new BigDecimal(500);
+    /***
+     * Método para criar cartão
+     * @param request
+     */
+    CartaoResponse criarCartao(CartaoRequest request) throws GenericBusinessException;
 
-    @Autowired
-    CartaoRepository repository;
+    /***
+     * Método para obter saldo disponível
+     * @param numeroCartao
+     */
+    BigDecimal obterSaldo(String numeroCartao) throws GenericBusinessException;
 
-    Object objError;
-
-    public CartaoResponse criarCartao(CartaoRequest request) throws GenericBusinessException {
-
-        try {
-
-            var cartao = repository.findById(request.getNumeroCartao());
-
-            cartao.ifPresent(c -> objError = CartaoResponse.builder()
-                    .numeroCartao(c.getId())
-                    .senha(c.getSenha())
-                    .build());
-
-            repository.insert(new Cartao(request.getNumeroCartao(), request.getSenha(), VALOR_SALDO_INICIAL));
-
-            return CartaoResponse.builder()
-                    .numeroCartao(request.getNumeroCartao())
-                    .senha(request.getSenha())
-                    .build();
-
-        } catch (Exception ex) {
-            throw new GenericBusinessException(HttpStatus.UNPROCESSABLE_ENTITY, objError);
-        }
-    }
-
-    public BigDecimal obterSaldo(String numeroCartao) throws GenericBusinessException {
-
-        var cartao = repository.findById(numeroCartao).orElseThrow(() -> new GenericBusinessException(HttpStatus.NOT_FOUND));
-
-        return cartao.getSaldo();
-    }
-
-    public void autorizar(TransacaoRequest request) throws GenericBusinessException {
-
-        var cartao = repository.findById(request.getNumeroCartao()).orElseThrow(() -> new GenericBusinessException(HttpStatus.NOT_FOUND, ErroAutorizacaoEnum.CARTAO_INEXISTENTE));
-        
-        if (!cartao.getSenha().equals(request.getSenha())) {
-            throw new GenericBusinessException(HttpStatus.UNPROCESSABLE_ENTITY, ErroAutorizacaoEnum.SENHA_INVALIDA);
-        }
-
-        if (cartao.getSaldo().compareTo(request.getValor()) >= 0) {
-            var saldoAtualizado = cartao.getSaldo().subtract(request.getValor());
-            cartao.setSaldo(saldoAtualizado);
-            repository.save(cartao);
-        } else {
-            throw new GenericBusinessException(HttpStatus.UNPROCESSABLE_ENTITY, ErroAutorizacaoEnum.SALDO_INSUFICIENTE);
-        }
-    }
+    /***
+     * Método para autorizar transação
+     * @param request
+     */
+    void autorizar(TransacaoRequest request) throws GenericBusinessException;
 }
